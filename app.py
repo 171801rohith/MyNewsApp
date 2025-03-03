66dded28c1053a709a6796bba9d641ed754d1374
ROHITH <rohith171801@gmail.com>
2025-03-03 20:21:20 +0530

    Welcome To my News App

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+from datetime import datetime
+from dotenv import load_dotenv
+import os
+import requests
+import json
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+
+
+def getDate():
+    return datetime.now().strftime("%Y-%D-%M")
+
+
+@app.route("/")
+def home():
+    return render_template("layout.html")
+
+
+@app.route("/result", methods=["POST", "GET"])
+def result():
+    query = request.form["query"]
+    if query != "":
+        url = f"https://newsapi.org/v2/everything?q={query}&from={getDate()}&sortBy=publishedAt&apiKey={os.getenv("NEWS_API")}"
+        req = requests.get(url)
+        return render_template("result.html", news=json.loads(req.text))
+    return render_template("layout.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
